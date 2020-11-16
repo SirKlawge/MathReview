770a9f219c2fceadc7d55019d78fd4929163ef74
Ventura <hirounm@gmail.com>
2020-11-15 21:56:32 -0700

    adding everything

--- src/MySet.java
+++ src/MySet.java
@@ -7,10 +7,72 @@
 import java.util.Set;
 import java.util.HashSet;
 
-public class MySet<E> {
-	private Set<E> set;
+public class MySet<E> extends HashSet<E>{
 	
-	public MySet() {
-		set = new HashSet<E>();
+	@Override
+	public String toString() {
+		String result = "{";
+		for(E item : this) {
+			result += item + ", ";
+		}
+		result = result.trim();
+		return result.substring(0, result.length() - 1) + "}";
 	}
+	
+	/**This method will produce the cartesian product between itself and another set.
+	 * @param other a MySet
+	 * @return */
+	public MySet<OrderedPair<E>> cartesianProduct(MySet<E> other){
+		return null;
+	}
+	
+	/**This method will return the powerset of this MySet
+	 * @return*/
+	public MySet<E> powerset(){
+		return null;
+	}
+	
+	/**This method return the intersection of this MySet and an other MySet
+	 * @param other another MySet
+	 * @return intersection the intersection of this MySet and another MySet*/
+	public MySet<E> intersection(MySet<E> other){
+		MySet<E> intersection = new MySet<E>();
+		for(E item : this) {
+			if(other.contains(item)) {
+				intersection.add(item);
+			}
+		}
+		return intersection;
+	}
+	
+	/**This method returns the union of this MySet and another MySet.
+	 * @param other another MySet
+	 * @return union the union of this MySet and another MySet*/
+	public MySet<E> union(MySet<E> other){
+		MySet<E> union = (MySet<E>) this;
+		for(E item : other) {
+			union.add(item);
+		}
+		return union;
+	}
+	
+	/**This will return a MySet such that x is in this or x is in other and x is
+	 * not in both.
+	 * @param other a MySet
+	 * @return xorSet a MySet as described above.*/
+	public MySet<E> xor(MySet<E> other){
+		MySet<E> xorSet = new MySet<E>();
+		for(E item : this) {
+			if(!other.contains(item)) {
+				xorSet.add(item);
+			}
+		}
+		for(E item : other) {
+			if(!this.contains(item)) {
+				xorSet.add(item);
+			}
+		}
+		return xorSet;
+	}
+	
 }
--- src/Testing.java
+++ src/Testing.java
@@ -1,9 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
 
 public class Testing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MySet<Integer> set1 = new MySet<Integer>();
+		set1.add(1);
+		set1.add(2);
+		MySet<Integer> set2 = new MySet<Integer>();
+		set2.add(2);
+		set2.add(23);
+		System.out.println(set1.xor(set2));
 	}
 
 }
